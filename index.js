ecf2231f5c5a28d923a7706c1f7d7944e8f46d86
kelly_maass <kmaass77627@gmail.com>
2024-11-24 20:09:53 -0600

    done

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let form = document.getElementById("form")
     form.addEventListener("submit", (e) => {
         e.preventDefault()
-        console.log(e.target.input.value)
+        // console.log(e.target.input.value)
         createListGroup(e.target.input.value)
     })
 
     let carousel = document.getElementById("myCarousel")
     let map = document.getElementById("usamap")
     let ol = document.getElementById("list-group")
+    let ul = document.getElementById("details")
 
     let button = document.getElementById("togglebutton")
     button.addEventListener("click", (e) => {
@@ -50,7 +51,6 @@ document.addEventListener("DOMContentLoaded", () => {
           console.log(parks)
 
        for (let i = 0; i < parks.length; i++) {
-        // let ol = document.getElementById("list-group")
              let listitem = document.createElement("li")
              listitem.className = "list-group-item"
              listitem.id = `${i}`
@@ -70,30 +70,46 @@ document.addEventListener("DOMContentLoaded", () => {
     
 
     function fillCarousel(e) {
-        console.log(imageone)
-        console.log("clicked")
-        console.log(e.target.innerHTML)
+        // console.log(imageone)
+        // console.log("clicked")
+        // console.log(e.target.innerHTML)
         let parkimage = document.getElementById("parkimage")
-         
         let name = e.target.innerHTML
         let park = globalData.find((park) => park.fullName == name)
-        console.log(park)
+        // console.log(park)
         let images = park.images
         let p = document.getElementById("p")
-        console.log(p)
+        // console.log(p)
         p.innerText = park.fullName
-        console.log(images)
+        // console.log(images)
         parkimage.src = images[4].url
-            imageone.src = images[0].url
-            imagetwo.src = images[1].url
-            imagethree.src = images[2].url
+        imageone.src = images[0].url
+        imagetwo.src = images[1].url
+        imagethree.src = images[2].url
 
-            parkInfo()
+            parkInfo(park)
+        }
 
-    }
+    function parkInfo(park) {
+        console.log(park.operatingHours[0].standardHours)
+        let hours = park.operatingHours[0].standardHours
+        console.log(park.activities[0].name)
+        // console.log(ul.childNodes[1])
+        // console.log(ul.childNodes[3])
+        // console.log(ul.childNodes[5])
+        // console.log(ul.childNodes[7])
+        // console.log(ul.childNodes[9])
+        // console.log(ul.li.parkname)
 
-    function parkInfo() {
-        
+        ul.childNodes[1].innerText = "Name:" + " " + park.name
+        ul.childNodes[3].innerText = park.states
+        ul.childNodes[5].innerText = "Hours: \n" + " Monday: " + hours.monday + "\n Tuesday: " + hours.tuesday +  "\n Wednesday: " + hours.wednesday + "\n Thursday: " + hours.thursday + "\n Friday: " + hours.friday
+        ul.childNodes[7].innerText = "Weather:" + " " + park.weatherInfo
+        // ul.childNodes[9].innerText = park.activities
+        for (let i = 0; i < park.activities.length; i++) {
+            ul.childNodes[9].innerText += park.activities[i].name + ",  "
+        }
+
 
     }
 
@@ -117,12 +133,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-// let globalData
-
-// async function myFunction () {
-//   const res = await fetch('./../JSON.json')
-//   const data = await res.json()
-//   globalData = data
-//   console.log(globalData)
-// }
-// myFunction()
